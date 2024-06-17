ae530dfef683e3344095d92cdda77290bbb5666c
robert <robertli0456@126.com>
2024-06-17 09:45:15 +0800

    with PLS

--- PLS.py
+++ PLS.py
@@ -12,12 +12,6 @@ scaler = StandardScaler()
 
 # 正常操作数据 (训练)
 X_train_normal = data['d00'].reshape(-1, 52)
-y_train_normal = np.zeros(X_train_normal.shape[0])
-X_train_normal_scaled = scaler.fit_transform(X_train_normal)
-
-# # 训练PLS模型用于正常数据
-# pls = PLSRegression(n_components=2)
-# pls.fit(X_train_normal_scaled, y_train_normal)
 
 # 初始化存储结果
 results = []
@@ -27,31 +21,30 @@ for i in range(1, 22):
     fault_type = f'd{i:02}'
 
     X_train_fault = data[fault_type].reshape(-1, 52)
-    X = np.vstack([X_train_normal, X_train_fault])
-    y_train_fault = np.concatenate([np.zeros(500), np.ones(480)])
-    X_scaled = scaler.fit_transform(X)
+    X = np.vstack([X_train_normal, X_train_fault])  # 训练集中的 正常+异常 合在一起
+    y_train = np.concatenate([np.zeros(500), np.ones(480)])
+    X_scaled = scaler.fit_transform(X)  # 合起来以后再标准化  此方法用于在训练集上标准化
 
     # 训练PLS模型用于故障数据
-    pls = PLSRegression(n_components=20)
-    pls.fit(X_scaled, y_train_fault)
+    pls = PLSRegression(n_components=40)
+    pls.fit(X_scaled, y_train)
 
     # 故障测试数据
     X_test_fault = data[f'{fault_type}_te'].reshape(-1, 52)
     y_test_fault = np.concatenate([np.zeros(160), np.ones(800)])  # 前160正常，后800故障
 
     # 标准化测试数据
-    X_test_fault_scaled = scaler.transform(X_test_fault)
+    X_test_fault_scaled = scaler.transform(X_test_fault)  # 此方法用于在测试集上标准化
 
     # 使用PLS模型进行故障检测
     y_pred_fault = pls.predict(X_test_fault_scaled)
-    y_pred_fault = (y_pred_fault[:, 0] > 0.4).astype(int)  # 假设阈值为0.2
-    # print("y_pred_fault = ", y_pred_fault)
+    y_pred_fault_label = (y_pred_fault[:, 0] > 0.4).astype(int)  # 假设阈值为0.4
 
     # 计算性能指标
-    TP = np.sum((y_pred_fault == 1) & (y_test_fault == 1))
-    FP = np.sum((y_pred_fault == 1) & (y_test_fault == 0))
-    TN = np.sum((y_pred_fault == 0) & (y_test_fault == 0))
-    FN = np.sum((y_pred_fault == 0) & (y_test_fault == 1))
+    TP = np.sum((y_pred_fault_label == 1) & (y_test_fault == 1))
+    FP = np.sum((y_pred_fault_label == 1) & (y_test_fault == 0))
+    TN = np.sum((y_pred_fault_label == 0) & (y_test_fault == 0))
+    FN = np.sum((y_pred_fault_label == 0) & (y_test_fault == 1))
 
     FAR = FP / (FP + TN) if (FP + TN) > 0 else 0
     FDR = TP / (TP + FN) if (TP + FN) > 0 else 0
@@ -59,8 +52,19 @@ for i in range(1, 22):
 
     results.append({'fault_type': fault_type, 'FAR': FAR, 'FDR': FDR, 'DD': DD})
 
-
     print(f"{fault_type}: FAR: {FAR}, FDR: {FDR}, DD: {DD}\n")
+
+    if FDR>0.7 and FAR<0.5:
+        plt.figure(i, figsize=(15, 6))
+        plt.plot(y_pred_fault, label='PLS Predicted Scores')
+        plt.axhline(y=0.4, color='red', label='Threshold', linestyle='--')
+        plt.title(f'PLS Prediction Scores for Fault Type {fault_type}')
+        plt.xlabel('Sample Index')
+        plt.ylabel('Predicted Score')
+        plt.legend()
+
+plt.show()
+
 """
 
 # 绘制结果
